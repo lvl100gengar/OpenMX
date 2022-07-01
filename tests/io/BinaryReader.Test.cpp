7175039359dde4467bf61d5cf33862dd02123100
sean <sean2436@hotmail.com>
2022-06-30 22:14:46 -0400

    Added Catch2 test header and first test suite

---
+++ tests/io/BinaryReader.Test.cpp
@@ -0,0 +1,91 @@
+#include "io/BinaryReader.h"
+#include "../catch.hpp"
+
+using OpenMX::io::BinaryReader;
+
+TEST_CASE("Reading a byte")
+{
+    const char buffer[] = {0x64};
+    BinaryReader reader = BinaryReader(buffer, 0, 1);
+
+    REQUIRE(reader.readByte() == 0x64);
+}
+
+TEST_CASE("Reading a short")
+{
+    const char buffer[] = {0x64, 0x64};
+    BinaryReader reader = BinaryReader(buffer, 0, 2);
+
+    REQUIRE(reader.readShort() == 0x6464);
+}
+
+TEST_CASE("Reading an int")
+{
+    const char buffer[] = {0x64, 0x64, 0x64, 0x64};
+    BinaryReader reader = BinaryReader(buffer, 0, 4);
+
+    REQUIRE(reader.readInt() == 0x64646464);
+}
+
+TEST_CASE("Reading a long")
+{
+    const char buffer[] = {0x64, 0x64, 0x64, 0x64, 0x64, 0x64, 0x64, 0x64};
+    BinaryReader reader = BinaryReader(buffer, 0, 8);
+
+    REQUIRE(reader.readLong() == 0x6464646464646464);
+}
+
+TEST_CASE("Reading several bytes")
+{
+    const char buffer[] = {1, 10, 100, (char)255};
+    BinaryReader reader = BinaryReader(buffer, 0, 4);
+    
+    unsigned char *result = reader.readBytes(3);
+
+    REQUIRE(result[0] == buffer[0]);
+    REQUIRE(result[1] == buffer[1]);
+    REQUIRE(result[2] == buffer[2]);
+    REQUIRE(reader.canRead(1));
+}
+
+TEST_CASE("Reading a null-term string")
+{
+    const char buffer[] = "str1\0str2";
+    BinaryReader reader = BinaryReader(buffer, 0, 10);
+    
+    std::string result1 = reader.readString();
+    std::string result2 = reader.readString();
+
+    REQUIRE(result1 == std::string(buffer));
+    REQUIRE(result2 == std::string(buffer + 5));
+}
+
+TEST_CASE("Reading a fixed-length string")
+{
+    const char* buffer = "abcdefg";
+    BinaryReader reader = BinaryReader(buffer, 0, 8);
+    
+    std::string result1 = reader.readString(3);
+
+    REQUIRE(result1 == std::string(buffer, 3));
+}
+
+TEST_CASE("Reading an unterminated string")
+{
+    const char* buffer = "string";
+    BinaryReader reader = BinaryReader(buffer, 0, 5);
+    
+    std::string result1 = reader.readString();
+
+    REQUIRE(result1 == std::string(buffer, 5));
+}
+
+TEST_CASE("Reading an empty string")
+{
+    const char* buffer = "";
+    BinaryReader reader = BinaryReader(buffer, 0, 1);
+    
+    std::string result1 = reader.readString();
+
+    REQUIRE(result1.empty());
+}
